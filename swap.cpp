c4fcc4549d12b998d1b0dc67ff16cd8ed428d588
imsatis <Malsonsatis@outlook.com>
2018-06-21 23:45:53 +0530

    swapping file added

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+  int main()
+{
+  int a,b;
+  cout<<"Enter The no = ";cin>>a;
+  cout<<"Enter The no = ";cin>>b;
+ 
+  a=a-b;
+
+
+
+  cout<<"\na is = "<<a;
+  cout<<"\nb is = "<<b;
+
+}  
