0d7f880a7c5ed31c9b205463fa5bd295f2f105a8
imsatis <Malsonsatis@outlook.com>
2018-06-27 09:41:42 +0530

    Addition object file add

---
+++ objadd.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+
+    class add
+{
+    int a,b;
+
+    public:
+
+      add(int x,int y)
+     {
+         a=x;
+	 b=y;
+          
+	  }   
+
+      add(add &o1,add &o2)
+     {
+       a=o1.a+o2.a;
+       b=o1.b+o2.b;
+
+        } 
+
+    void show()
+   { 
+      cout<<"\n"<<a;
+      cout<<"\n"<<b;
+      }  
+
+   };    
+
+
+   int main()
+ {
+   int a,b,c,d;
+   cin>>a>>b>>c>>d;
+    add ob1(a,b);
+    add ob2(c,d);
+    add ob3(ob1,ob2);
+
+    ob1.show();
+
+    ob2.show();
+
+    ob3.show();
+    getch();
+     }  
+
+
