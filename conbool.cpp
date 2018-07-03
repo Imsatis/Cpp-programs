92851fd6e941d6325c91e1091147acc791cdd95a
imsatis <Malsonsatis@outlook.com>
2018-07-03 22:31:38 +0530

    return bool

---
+++ conbool.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<conio.h>
+
+
+ using namespace std;
+
+   class con
+{
+    public:
+   
+
+     void chk(int x,int y)
+    { 
+      if(x<10&&y<10)
+     { int r;
+      r=x+y;
+      cout<<"\nr "<<r;
+         }
+       else 
+       //return 0;
+
+       }   
+
+     };
+
+        int main()
+  {
+     int a=0,b=0;
+     con obj;
+     
+     while(obj.chk(a,b))
+     { 
+     a++;b++;
+     obj.chk(a,b);
+     //cout<<"\nob "<<obj;
+     getch();
+     
+        }
+  
+       }	
