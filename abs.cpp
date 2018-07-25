dc2d508dbe52f897a3b896f895963124eec40ca3
imsatis <Malsonsatis@outlook.com>
2018-07-25 18:57:49 +0530

    coding

---
+++ abs.cpp
@@ -0,0 +1,34 @@
+/* Abstract Data Type */
+
+/* Testing Operator */
+
+#include<iostream>
+
+using namspace std;
+
+   class abs {
+      
+      int a,b,r;
+      public:
+
+      abs() {
+        
+	a=1;
+	b=1;
+      }
+      abs(int x,int y) {
+         
+         a=x;
+	 b=y;
+        
+      }
+      
+    abs operator+(abs cob){
+
+       abs tob;
+
+        
+
+      }
+     
+   }
