7fc57ec91f4953c3835169c9ea87c060d0746a00
imsatis <Malsonsatis@outlook.com>
2018-07-28 08:53:33 +0530

    logic testing

--- My.cpp
+++ My.cpp
@@ -1,37 +1,59 @@
 #include<iostream>
-#include<fstream>
+#include<conio.h>
 
 
 using namespace std;
 
-  class bi {
+ class bi {
 
-  int a,b;
-     
-     public:
+ int a,b;
+      
+  public:    
      int convert(int,int);
      void show();
     
   };
 
   int bi::convert(int x,int y) {    //function overlaoding
-    return x+y;
+    char v[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},i=0;
+    for(;y>=1;) {
+        
+	v[i]=y%x;
+	i++;
+	y=y/x;
+//	getch();
+
+     }  
+     for(i=15;i>=0;i--) { cout<<(int)v[i];}
+
+
+//     return x+y;
 
   }
 
-  float convert(float x,float y) {  //overloading 
+/*  float convert(float x,float y) {  //overloading 
     
     
     return y%x;
   
+  }*/
+
+
+  void bi::show() { 
+  bi o;
+  o.a=10;
+
+  cout<<"\n"<<o.a<<"  "<<o.b<<"\n" ;
+
   }
-
-
-  void show() {}
 
   int main() {
      bi ob;
     
-   cout<<convert(2,11)<<"\n"<<ob.convert(10,10);
-  
+ //  cout<<convert(2,11)<<"\n";
+ //   cout<<
+    ob.convert(2,16674);
+//     cout<<
+  // ob.show();
+
   }
